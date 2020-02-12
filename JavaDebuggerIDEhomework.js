33b095f91a1cca2e24b526030c2a12710e39815d
Steve Vance <steve.vance@hotmail.co.uk>
2020-02-12 19:40:30 +0000

    added .js file for readability

---
+++ JavaDebuggerIDEhomework.js
@@ -0,0 +1,18 @@
+
+
+What is the purpose of a breakpoint?
+
+Placing a breakpoint within the programme allows us to 	intentionally create an intentional stop or pause point within the programme for debugging purposes. It is also a means of acquiring further knowledge about what a programme is doing during its execution. It is placed on the line that we want the application to pause.
+
+Does the line of code on a breakpoint run when you start debugging?
+No, the code will run up to the point that the breakpoint is placed and will then pause the application and launch the debugger console.
+
+How do we debug the next line of code?
+We can use the ‘step over’ command to run the breakpoint line of code and then automatically run the next line of code.
+
+What does the step into command do?
+The step into command allows you to step into and see what is happening within specific methods in the code. It will pause at the top of the method. It can also open up classes within those methods.
+
+What is the difference between evaluate expression and evaluate code fragment?
+Evaluate Expression allows you to evaluate and run java code in the console and call methods on the instances and objects that were created in the code. You can’t use semi colons in the console.
+Evaluate Code Fragment allows you to evaluate multiple methods and lines of code and create temporary variables to store things. The code in ECF can be run over and over and the variables remain assigned until the console is closed. You have to use semi colons in the console
